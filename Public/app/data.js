a6cef7194c6b4ad86b08a15d5442f05837dad194
noczero <zeroonetm@gmail.com>
2017-08-20 21:05:22 +0700

    fix sikap

--- Public/app/data.js
+++ Public/app/data.js
@@ -75,7 +75,7 @@ function update() {
 
        // console.log((360.00-Pitch) + ' ' +  (360.00-Yaw) + ' ' + (360.00-Roll));
         //console.log((Pitch) + ' ' +  (Yaw) + ' ' + (Roll));
-        box.rotation((360.00-Pitch), (360.00-Yaw), (360.00-Roll)).update();
+        box.rotation((Pitch), (360-Yaw), (Roll)).update();
        //box.rotation((Pitch), (Yaw), (Roll)).update();
 
         });
